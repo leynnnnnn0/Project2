e8f2484b608d50c678e43cbd273a93511bd9d857
M-Juss <m.justinsayson@gmail.com>
2026-03-03 00:07:05 +0800

    implement storing of user deets in shared preferences using model / json

--- app/src/main/java/com/example/project2/helpers/SessionHelper.java
+++ app/src/main/java/com/example/project2/helpers/SessionHelper.java
@@ -3,16 +3,60 @@ package com.example.project2.helpers;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.example.project2.models.UserModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SessionHelper {
-    public static final String KEY_USER_ID = "USER_ID";
+    public  static  final  String USER_FOLDER = "user_session";
+    public  static  final  String KEY_USER = "KEY_USER";
 
-    public void setUserId(Context context, int userId){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(KEY_USER_ID, Context.MODE_PRIVATE);
-        sharedPreferences.edit().putInt(KEY_USER_ID, userId).apply();
+    public  static void setUser(Context context, UserModel user){
+        JSONObject json = new JSONObject();
+        try {
+            json.put("id", user.getId());
+            json.put("fullname", user.getFullname());
+            json.put("email", user.getEmail());
+            json.put("password", user.getPassword());
+            json.put("role", user.getRole());
+
+            SharedPreferences sharedPreferences = context.getSharedPreferences(USER_FOLDER, Context.MODE_PRIVATE);
+            sharedPreferences.edit().putString(KEY_USER, json.toString()).apply();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public int getUserId (Context context){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(KEY_USER_ID, Context.MODE_PRIVATE);
-        return sharedPreferences.getInt(KEY_USER_ID, -1);
+    public static UserModel getUser(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(USER_FOLDER, Context.MODE_PRIVATE);
+        String jsonString = sharedPreferences.getString(KEY_USER, null);
+
+        try {
+            JSONObject json = new JSONObject(jsonString);
+                return new UserModel(
+                        json.getInt("id"),
+                        json.getString("fullname"),
+                        json.getString("email"),
+                        json.getString("password"),
+                        json.getString("role")
+                );
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
 }
+
+
+//public void setUserId(Context context, int userId){
+//    SharedPreferences sharedPreferences = context.getSharedPreferences(KEY_USER_ID, Context.MODE_PRIVATE);
+//    sharedPreferences.edit().putInt(KEY_USER_ID, userId).apply();
+//}
+//
+//public int getUserId (Context context){
+//    SharedPreferences sharedPreferences = context.getSharedPreferences(KEY_USER_ID, Context.MODE_PRIVATE);
+//    return sharedPreferences.getInt(KEY_USER_ID, -1);
+//}
--- app/src/main/java/com/example/project2/models/UserModel.java
+++ app/src/main/java/com/example/project2/models/UserModel.java
@@ -1,12 +1,57 @@
 package com.example.project2.models;
 
 public class UserModel {
-    public String firstName, email;
+
+
+    public String fullname, email, password, role;
     public int id;
 
-    public UserModel(int id, String firstName, String email) {
+
+    public UserModel(int id, String fullname, String email, String password, String role) {
         this.id = id;
-        this.firstName = firstName;
+        this.fullname = fullname;
         this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+
+    public UserModel(int id, String fullname, String email) {
+        this.id = id;
+        this.fullname = fullname;
+        this.email = email;
+    }
+
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
